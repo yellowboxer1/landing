cd72de078e1f939649830fd538429a82e25ad863
gunho <gunho@gunhoui-MacBookPro.local>
2025-09-12 22:34:18 +0900

    server

---
+++ src/assets/js/landing/email.init.js
@@ -0,0 +1,10 @@
+/*
+Template Name: Domiex - Admin & Dashboard Template
+Author: SRBThemes
+Version: 1.0.0
+File: email landing init Js File
+*/
+
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+AOS.init();
